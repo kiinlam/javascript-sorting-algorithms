a8443e4b2a9a1b5ffacd210ab434f114c80d14cb
kiinlam <516523522@qq.com>
2019-04-08 23:54:56 +0800

    Update QuickSortSinglePivot.js

--- QuickSortSinglePivot.js
+++ QuickSortSinglePivot.js
@@ -8,39 +8,10 @@
  */
 function quickSortSinglePivot(array = [], leftIndex = 0, rightIndex = array.length - 1) {
 
-    // 分区数组
-    function partition(lowIndex, highIndex) {
-        // 交换数组元素
-        function swap(left, right) {
-            if (left !== right) {
-              [array[left], array[right]] = [array[right], array[left]]
-            }
-        }
-
-        // 用于比较的基准值
-        // 最终小于基准值的元素排在左边，大于等于基准值的元素排在右边
-        const pivot = array[highIndex]
-
-        // 设置分区位置初始值
-        let partitionIndex = lowIndex
-        // 逐个于基准值比较，小于基准值时，将元素与分区位置的元素交换，同时分区位置往右移
-        for (let currentIndex = lowIndex; currentIndex < highIndex; currentIndex++) {
-            if (array[currentIndex] < pivot) {
-                swap(partitionIndex, currentIndex)
-                partitionIndex += 1
-            }
-        }
-
-        // 将分区位置的元素与基准值交换
-        swap(partitionIndex, highIndex)
-
-        return partitionIndex
-    }
-
     // 数组有两个或以上元素才需要排序
     if (leftIndex < rightIndex) {
         // 分区位置
-        const partitionIndex = partition(leftIndex, rightIndex)
+        const partitionIndex = partition(array, leftIndex, rightIndex)
         // 递归排序
         quickSortSinglePivot(array, leftIndex, partitionIndex - 1)
         quickSortSinglePivot(array, partitionIndex + 1, rightIndex)
@@ -48,6 +19,36 @@ function quickSortSinglePivot(array = [], leftIndex = 0, rightIndex = array.leng
 
     // 返回排序完的数组副本
     return array
+}
+
+// 分区数组
+function partition(array, lowIndex, highIndex) {
+
+    // 用于比较的基准值
+    // 最终小于基准值的元素排在左边，大于等于基准值的元素排在右边
+    const pivot = array[highIndex]
+
+    // 设置分区位置初始值
+    let partitionIndex = lowIndex
+    // 逐个于基准值比较，小于基准值时，将元素与分区位置的元素交换，同时分区位置往右移
+    for (let currentIndex = lowIndex; currentIndex < highIndex; currentIndex++) {
+        if (array[currentIndex] < pivot) {
+            swap(partitionIndex, currentIndex)
+            partitionIndex += 1
+        }
+    }
+
+    // 将分区位置的元素与基准值交换
+    swap(partitionIndex, highIndex)
+
+    return partitionIndex
+}
+
+// 交换数组元素
+function swap(array, left, right) {
+    if (left !== right) {
+      [array[left], array[right]] = [array[right], array[left]]
+    }
 }
 
 quickSortSinglePivot( [5, 3, 7, 4, 1, 9, 8, 6, 2]) // =>[1, 2, 3, 4, 5, 6, 7, 8, 9]
